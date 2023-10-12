393b5b7288d4c73d7adc108fb2f2dc9e67fefc71
Saviorrrrrrr <147582602+Saviorrrrrrr@users.noreply.github.com>
2023-10-12 16:24:11 +0700

    Add files via upload

---
+++ src/main/WindowGame.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
+
+import javax.swing.JFrame;
+
+public class WindowGame {
+	private JFrame jframe;
+	public WindowGame (GamePanel  gamePanel) {
+		
+		jframe = new JFrame();
+		
+		
+		
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		jframe.add(gamePanel);
+		
+		jframe.setLocationRelativeTo(null);
+		
+		jframe.setResizable(false); // it dont resize terrible
+		
+		jframe.pack(); // make window enough to fit panel
+		
+		jframe.setVisible(true);
+		
+		jframe.addWindowFocusListener(new WindowFocusListener() {
+			
+			@Override // print out sthing when close window
+			public void windowLostFocus(WindowEvent e) {
+				gamePanel.getGame().windowForusLost();;
+			}
+			@Override
+			public void windowGainedFocus(WindowEvent e) {
+				
+			}
+		});
+	}
+
+}
