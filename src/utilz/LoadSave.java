cd09b091f92807a5ab30087900ddfd26154e73ed
Nghia <trinhvantrungnghia@gmail.com>
2023-11-28 14:27:40 +0700

    change at LoadSave.class

--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -12,7 +12,7 @@ import main.Game;
 
 public class LoadSave {
 
-	public static final String PLAYER_ATLAS = "player_sprites.png";
+	public static final String PLAYER_ATLAS = "adventurer_Sprite_Sheet.png";
 	public static final String LEVEL_ATLAS = "outside_sprites.png";
 //	public static final String LEVEL_ONE_DATA = "level_one_data.png";
 	public static final String LEVEL_ONE_DATA = "level_one_data_long.png";
